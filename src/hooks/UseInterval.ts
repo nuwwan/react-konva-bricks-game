d1232355ccfac28480137293b0e8de0db67d16fd
Nuwan Rathnayaka <nuwanrathnayaka@Nuwans-iMac.local>
2024-06-17 21:34:38 +0800

    add useinterval hook

---
+++ src/hooks/UseInterval.ts
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+export function useInterval(callback: () => void, delay: number | null): void {
+  const callbackRef = useRef(callback);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay == null) {
+      return;
+    }
+    const interval = setInterval(() => callbackRef.current(), delay);
+    return () => clearInterval(interval);
+  }, [delay]);
+}
